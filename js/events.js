3d82e9d8fdb13a509425d71192d934ea2a61e9d2
Tu An Huynh Van <arishuynhvan@gmail.com>
2016-12-01 13:00:21 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,31 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+submitIt();
+pressIt();
 
 });
+function getIt(){
+  $('p').on('click',function(){
+    alert('Hey!');
+  })
+}
+function frameIt(){
+  $(document).on('load',function(){
+    $('img').addClass('tasty')
+  })
+}
+function submitIt(){
+  $('form').on('submit',function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
+function pressIt(){
+  $('#typing').on('keydown',function(key){
+    if(key.which =='G'.charCodeAt(0)){
+        alert('g was pressed');
+    }
+  })
+}
